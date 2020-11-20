2c12d609cc9847fdccd8615b354d6e9581edfeca
Louis Leffler <louisleffler@gmail.com>
2020-11-20 19:34:53 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -5,15 +5,11 @@ def addition(num1, num2)
 end
 
 def subtraction(num1, num2)
- num1 = 10 
- num2 = 5 
- puts (num1-num2)
+
 end
 
 def division(num1, num2)
- num1 = 50
- num2 = 2 
- puts
+  
 end
 
 def multiplication(num1, num2)
