50f56af90ded5a0a81fc6ccc7b604387988cf0df
Louis Leffler <louisleffler@gmail.com>
2020-11-20 19:21:38 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -1,19 +1,19 @@
 def addition(num1, num2)
   num1 = 5
   num2 = 4 
- puts (num1 + num2)
+ puts (num1+num2)
 end
 
 def subtraction(num1, num2)
  num1 = 10 
  num2 = 5 
- puts (num1 - num2)
+ puts (num1-num2)
 end
 
 def division(num1, num2)
  num1 = 50
  num2 = 2 
- puts (num1 / num2 )
+ puts (num1/num2 )
 end
 
 def multiplication(num1, num2)
